4dffdd61b15d20323560cad7b8026c436087416b
ma-foyez <mafoyez-bd@gmail.com>
2020-08-07 00:35:39 +0600

    ES6 class property practise

---
+++ class.js
@@ -0,0 +1,12 @@
+//class use kore eky rokom onek gulo object declare kora jay
+
+class Student {
+    constructor(sId, sName) {
+        this.id = sId;
+        this.name = sName;
+        this.school = "North Charlakshya Government Primary School";
+    }
+}
+const student1 = new Student(1, "Fayez");
+const student2 = new Student(2, "Giasuddin");
+console.log(student1, student2);
