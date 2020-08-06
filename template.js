bf3d64bc25f580a278669ca5999cb3b2cecdba84
ma-foyez <mafoyez-bd@gmail.com>
2020-08-06 16:02:58 +0600

    ES6 template

---
+++ template.js
@@ -0,0 +1,9 @@
+const firstName = "MD";
+const lastName = "Meheraz";
+// const fullName = firstName + " " + lastName;
+//ES6 template below
+// const fullName = `M.A Fayez is a good boy.`;
+const fullName = `${firstName} ${lastName} is a good boy.
+He writes stories.
+He is a student of CU.`
+console.log(fullName);
